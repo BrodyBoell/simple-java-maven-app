e38803d58b02719eca2bcb0cf82e9077f4defeb9
Brody Boell <bjboell1@dmacc.edu>
2024-09-29 19:24:49 -0500

    Add comments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,15 +4,16 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App {
-
+    // A constant string message that will be printed to the console
     private static final String MESSAGE = "Hello World!";
-
+    // Constructor for the App class
     public App() {}
-
+    // Main method
     public static void main(String[] args) {
+                // Print the message constant to the console
         System.out.println(MESSAGE);
     }
-
+    // Method to retrieve the message string
     public String getMessage() {
         return MESSAGE;
     }
